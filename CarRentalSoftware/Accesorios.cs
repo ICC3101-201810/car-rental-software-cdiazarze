ce25526eac3361fb66a9d55d8c443c6d88f71739
Carlos Alberto Diaz Arze <cdiaz1@miuandes.cl>
2018-04-03 20:03:15 -0300

    Crecion de clases

---
+++ CarRentalSoftware/Accesorios.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSoftware
+{
+    class Accesorios
+    {
+        string nombre;
+        float precio;
+       
+        public Accesorios(string miNombre, float miPrecio)
+        {
+            nombre = miNombre;
+            precio = miPrecio;
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+        public float Precio
+        {
+            get
+            {
+                return this.precio;
+            }
+            set
+            {
+                this.precio = value;
+            }
+        }
+
+    }
+
+}
---
+++ CarRentalSoftware/Sucursal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSoftware
+{
+    class Sucursal
+    {
+        List<Vehiculos> Vehiculos= new List<Vehiculos>();
+        List<Accesorios> Accesorios = new List<Accesorios>();
+        int capacidad;
+
+        public Sucursal(int miCapacidad)
+        {
+            capacidad = miCapacidad;
+        }
+
+        public bool AgregarVehiculos(Vehiculos vehiculo)
+        {
+            if (capacidad > 0)
+            {
+                Vehiculos.Add(vehiculo);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
